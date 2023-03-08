aa35135550a3c53cb957f8bb319468f86b25d59e
Ejemy <eric.iwan@mail.com>
2023-03-08 07:14:01 +0000

    quality of eyes

--- script.js
+++ script.js
@@ -19,18 +19,14 @@ $(document).ready(function () {
         );
     }
   });
-
-  $("img").each(function(index, ele){
-    var arr = [];
-    arr.push($(ele).position().top);
-    console.log(arr)
-    //console.log($(ele).position().top)
-
-  })
-
+  
 
   $(document).on("scroll", function (e) {
     var top = $("#image").position().top;
+
+   
+
+
 
     if ($(document).scrollTop() > 300) {
       $(".wrapper").addClass("show");
@@ -47,20 +43,31 @@ $(document).ready(function () {
       );
     });
 
-    $("img").each(function(index, ele){
-      var arr = [];
-      //console.log(index)
-      //arr[index].push($(ele).position().top);
-      //console.log(arr)
-      //console.log($(ele).position().top)
-      if($(document).scrollTop() - $(ele).position().top >= -300){
-      $("img").css("filter", "blur(3px)");
-      $(".work p").css({"top": "-30%",
+
+
+
+    $(".about-works img").each(function(){
+
+      var screenhalf = $(window).height()/2;
+      var imgtop =$(this).offset().top - $(document).scrollTop();
+      var half = imgtop - screenhalf;
+      var blurstrength = (half / -50) / 2;
+      
+      
+      if(half < -100){
+       $(this).css({
+        "filter": "blur(3px)",
+        "opacity": "0.5"
+    });;
+      $(this).siblings("p").css({"top": "-30%",
         "opacity": 1,
         "padding": "2em"});
       } else{
-        $("img").css("filter", "blur(0)");
-        $(".work p").css({"top": "-20%",
+        $(this).css({
+          "filter": "blur(0px)",
+          "opacity": "0.5"
+      });
+        $(this).siblings("p").css({"top": "-20%",
         "opacity": 0,
         "padding": "1em"});
       }
@@ -71,12 +78,12 @@ $(document).ready(function () {
 
   $(".wrapper2").click(function (e) {
     $(".contact-me").addClass("show-me");
-    $("html, body").addClass("blur");
-    $(".contact-me").removeClass("blur");
+    $("body >*:not(.contact-me)").addClass("blur");
+    
   });
   $("#close").on("click", function (e) {
     $(".contact-me").removeClass("show-me");
-    $("html, body").removeClass("blur");
+    $("body >*:not(.contact-me)").removeClass("blur");
   });
 
   /*setInterval(() => {
